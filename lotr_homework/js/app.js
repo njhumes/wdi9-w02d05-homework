cffa6299547f7ccc61b265510ba6000e75361576
Nicholas Humes <njhumes@gmail.com>
2018-12-24 17:08:26 -0500

    Chapter 4 completed - Made the ring and gave it to Frodo

--- lotr_homework/js/app.js
+++ lotr_homework/js/app.js
@@ -88,16 +88,14 @@ const makeHobbits = () => {
 // ============
 const keepItSecretKeepItSafe = () => {
 
-  // 1. create an empty div with an id of 'the-ring'
-
-  // 2. add the ring as a child of Frodo
-
-  // hint: Frodo does not have an id, but there is a command to retrieve all elements with a certain class. This should give you an array for you to access . . .
-
-  // when you think you have given Frodo the ring, check in your Elements tab
-
+  let listOfHobbits = $('li').get(0);
+  console.log(listOfHobbits)
+  $(listOfHobbits).append('<div id="the-ring">The Ring</div>');
 };
-
+  // 1. create an empty div with an id of 'the-ring'
+  // 2. add the ring as a child of Frodo
+  // hint: Frodo does not have an id, but there is a command to retrieve all elements with a certain class. This should give you an array for you to access . . .
+  // when you think you have given Frodo the ring, check in your Elements tab
 // COMMIT YOUR WORK
 // The commit message should read: "Chapter 3 complete - Made the ring and gave it to Frodo".
 
